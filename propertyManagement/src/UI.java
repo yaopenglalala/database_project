495138e4023fc69097fc8de4dedbdf603a0bb138
zhengyuxin <16302010013@fudan.edu.cn>
2019-01-04 18:54:54 +0800

    UI

---
+++ propertyManagement/src/UI.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by your dad on 2019/1/4.
+ */
+public class UI {
+    public static void main(String[] args) {
+        System.out.println("welcome to Community System");
+        while (true){
+            System.out.println("1.房屋管理信息");
+            System.out.println("2.车位管理信息");
+            System.out.println("3.");
+
+            System.out.println("请输入要选择的数字，返回上一层用0");
+            Scanner scanner = new Scanner(System.in);
+            String   input = scanner.nextLine();
+
+        }
+
+
+    }
+}
